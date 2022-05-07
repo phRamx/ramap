8909dce671749d98371298f20cd9733820d08383
phRamx <105112215+phRamx@users.noreply.github.com>
2022-05-07 22:13:42 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => res.send('Merhaba Dünya!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
